2a0531210f7c89cf2ca6a3992dce44a6e7fb70d1
Carlos <carloshernando18@gmail.com>
2020-12-13 11:09:34 -0500

    buil: Creación del proyecto del backend y configuración de la base de datos

---
+++ Dynamic-Form-Generation-Api/src/main/java/com/dynamicformgeneration/DynamicFormGenerationApiApplication.java
@@ -0,0 +1,13 @@
+package com.dynamicformgeneration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicFormGenerationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicFormGenerationApiApplication.class, args);
+	}
+
+}
---
+++ Dynamic-Form-Generation-Api/src/test/java/com/dynamicformgeneration/DynamicFormGenerationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dynamicformgeneration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicFormGenerationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
