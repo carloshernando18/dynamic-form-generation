3ee61e576200d59fd940f485200ff94e9f268105
Carlos <carloshernando18@gmail.com>
2020-12-13 11:14:08 -0500

    feat: creación de la entidad de usuario

---
+++ Dynamic-Form-Generation-Api/src/main/java/com/dynamicformgeneration/entities/User.java
@@ -0,0 +1,73 @@
+package com.dynamicformgeneration.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "User", schema = "public")
+public class User {
+	
+	@Id
+	@GeneratedValue()
+	private Integer id;
+
+	@Column(nullable = false)
+	private String username;
+	
+	@Column(nullable = false)
+	private String password;
+	
+	@Column(nullable = false)
+	private Date createdDate;
+	
+	@Column(nullable = false)
+	private Date modifiedDate;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public Date getModifiedDate() {
+		return modifiedDate;
+	}
+
+	public void setModifiedDate(Date modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
+	
+	
+
+}
